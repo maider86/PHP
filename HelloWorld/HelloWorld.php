e093f3e5600dc6275ec2a5810a460dd30c0c4967
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-24 09:15:43 +0200

    Hello World

--- HelloWorld/HelloWorld.php
+++ HelloWorld/HelloWorld.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="ISO-8859-1">
-		<title>My first web page with PHP</title>
-	</head>
-	<body>
-		<?php
-			echo "Welcome to my first web page with PHP";
-		?>
-	</body>
-</html>
+<?php
+	echo "Welcome to my first web page with PHP.";
+	echo " ";
+	echo 123;
+	print " ";
+	print "Welcome to my first web page with PHP.";
+?>
