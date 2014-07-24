f368d68dde7057d02ea183a1c0aa36ee7e3dbdbd
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-24 09:33:39 +0200

    Interacción de PHP con HTML

---
+++ PHPWithHTML/PHPWithHTML.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Interacción con HTML</title>
+	</head>
+	
+	<body>
+		Hola HTML
+		<?php
+			echo "<br />Hola PHP";
+			echo "&nbsp;&nbsp;&nbsp;mi nombre es Maider";
+		?>
+	</body>
+</html>
