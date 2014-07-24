b86f4754f088f97c3006f2c759a95accf601d2e9
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-24 09:47:21 +0200

    Ejemplo de variables

---
+++ Variables/Variables.php
@@ -0,0 +1,17 @@
+<?php
+	//Los comentarios se escriben de esta manera
+	
+	/*
+	 * De esta manera también se pueden hacer comentarios
+	 */
+
+	$numero = 50;
+	$texto = "<br />Esto es un texto";
+	$booleanFalse = false;
+	$booleanTrue = true;
+
+	echo $numero;
+	echo $texto;
+	echo "<br />Valor Booleana False: $booleanFalse";
+	echo "<br />Valor Booleana True: $booleanTrue";
+?>
