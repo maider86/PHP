5549f85ca636851387583a8564523d1ff7ee8a5a
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-24 10:12:05 +0200

    Ejemplo de Vectores

---
+++ Vectores/Vectores.php
@@ -0,0 +1,13 @@
+<?php
+
+	//Arrays predefinidos
+	$array = array("elemento1", 2, "elemento 3");
+	
+	echo "Valores de los arrays predefinidos: ".$array[0].",&nbsp;".$array[1].",&nbsp;".$array[2]."<br />";
+
+	//Arrays asociativos o personalizados
+	$asociativo = array("clave1" => "elemento 1", "clave2" => 2);
+	
+	echo "Valores de los arrays asociativos o personalizados: ".$asociativo["clave1"].", &nbsp;".$asociativo["clave2"];
+	
+?>
