14c74db649647c6932193fe86be75325923da445
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-28 13:03:41 +0200

    Ejemplo del bucle For

---
+++ BucleFor/BucleFor.php
@@ -0,0 +1,15 @@
+<?php
+
+	echo "Dentro del For incremental: ";
+	for ($i = 1; $i < 5; $i++){
+		echo $i."<nbsp />";
+	}
+	
+	echo "<br/>";
+	
+	echo "Dentro del For decremental: ";
+	for ($i = 5; $i > 1; $i--){
+		echo $i."<nbsp />";
+	}
+
+?>
