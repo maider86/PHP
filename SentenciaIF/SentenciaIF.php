6cb689f424230664aebe0b66968cb9fe3b81741b
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-28 12:24:36 +0200

    Ejemplo de Sentencia IF

---
+++ SentenciaIF/SentenciaIF.php
@@ -0,0 +1,13 @@
+<?php
+
+	if ((8 == 7) && (7 == 6)) {
+		echo "La primera condición es verdad";
+	}
+	elseif (6 == 5) {
+		echo "La segunda condición es verdad";
+	}
+	else {
+		echo "Ninguna de las condiciones anteriores es verdad";
+	}
+
+?>
