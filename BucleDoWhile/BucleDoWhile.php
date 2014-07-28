1a9469c40046c9e4b76c137cb49422c09adddeba
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-28 12:46:11 +0200

    Ejemplo del bucle Do-While

---
+++ BucleDoWhile/BucleDoWhile.php
@@ -0,0 +1,14 @@
+<?php
+
+	$numero1 = 10;	
+
+	do {
+		echo "Bucle Do-While: ".$numero1;
+	}while ($numero1 < 9);
+	
+	$numero2 = 10;
+	while ($numero2 < 9){
+		echo "Bucle While: ".$numero2;
+	}
+
+?>
