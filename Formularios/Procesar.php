527de1aba41f0156d4beda085299d7cd2f28740a
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-30 16:32:12 +0200

    Ejemplo de un Formulario

---
+++ Formularios/Procesar.php
@@ -0,0 +1,11 @@
+<?php
+
+	$nombre = $_GET['nombre'];
+	$apellido = $_GET['apellido'];
+	$password = $_GET['password'];
+	
+	echo "Nombre: ".$nombre."<br />";
+	echo "Apellido: ".$apellido."<br />";
+	echo "Password: ".$password;
+
+?>
