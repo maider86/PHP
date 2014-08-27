b9d23e63ef816a7c4c1b5c1a60ab4a8057616d6d
Maider de Jesus <maiderdejesus@gmail.com>
2014-08-27 17:52:10 +0200

    Cambio en el Title del Formulario.php

--- BaseDeDatos/Formulario.php
+++ BaseDeDatos/Formulario.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Formulario para insertar datos en una Base de Datos</title>
+		<title>Formulario para Insertar/Seleccionar/Eliminar datos en una Base de Datos</title>
 	</head>
 	
 	<body>
